4e55badec856536411cf0c5a5134314428d32c83
Martin Lipták <mliptak@gmail.com>
2015-06-10 12:17:35 +0200

    Use specs

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,23 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require "minitest/spec"
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+
+  # Specs
+  
+  class << self
+    remove_method :describe
+  end
+
+  extend MiniTest::Spec::DSL
+
+  register_spec_type self do |desc|
+    desc < ActiveRecord::Base if desc.is_a? Class
+  end
 
   # Add more helper methods to be used by all tests here...
 end
