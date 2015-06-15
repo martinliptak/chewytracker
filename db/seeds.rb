54c577787005fced512559df8101734f6dd93acd
Martin Lipták <mliptak@gmail.com>
2015-06-15 15:22:18 +0200

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+user = User.create! name: "Regular user", email: "regular-user@example.com", password: "secret", password_confirmation: "secret"
+50.times { |i| 
+  Meal.create!(user: user, name: "Meal #{i + 1}", calories: 250, eaten_at: Time.now - i.days)
+}
+
+User.create! name: "User manager", email: "user-manager@example.com", password: "secret", password_confirmation: "secret", role: "user_manager"
+User.create! name: "Admin", email: "admin@example.com", password: "secret", password_confirmation: "secret", role: "admin"
