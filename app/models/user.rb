d9cdf27ecee924858e68dbdb537d0d4946e4e36b
Martin Lipták <mliptak@gmail.com>
2015-06-16 10:29:15 +0200

    Validate password length

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@ class User < ActiveRecord::Base
   
   validates_presence_of :password, on: :create
   validates_confirmation_of :password, on: :create
+  validates_length_of :password, within: 5..40, allow_blank: true
 
   has_many :meals, dependent: :destroy
   has_many :access_tokens, dependent: :destroy
