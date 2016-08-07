590ffc12a51e1fecdfb5b2e4797673f2927a413a
Martin Lipták <mliptak@gmail.com>
2016-08-07 15:13:01 +0200

    Add new meal form

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -4,12 +4,24 @@ angular
     '$scope',
     function($scope) {
       $scope.meals = [
-        {name: 'Dinner on Saturday', calories: 250, when: '06 Aug 18:00'},
-        {name: 'Lunch on Saturday', calories: 250, when: '06 Aug 12:00'},
-        {name: 'Breakfast on Saturday', calories: 250, when: '06 Aug 09:00'},
-        {name: 'Dinner on Friday', calories: 250, when: '06 Aug 18:00'},
-        {name: 'Lunch on Friday', calories: 250, when: '06 Aug 12:00'},
-        {name: 'Breakfast on Friday', calories: 250, when: '06 Aug 09:00'}
+        {name: 'Dinner on Saturday', calories: 250, eatenAt: '06 Aug 18:00'},
+        {name: 'Lunch on Saturday', calories: 250, eatenAt: '06 Aug 12:00'},
+        {name: 'Breakfast on Saturday', calories: 250, eatenAt: '06 Aug 09:00'},
+        {name: 'Dinner on Friday', calories: 250, eatenAt: '06 Aug 18:00'},
+        {name: 'Lunch on Friday', calories: 250, eatenAt: '06 Aug 12:00'},
+        {name: 'Breakfast on Friday', calories: 250, eatenAt: '06 Aug 09:00'}
       ];
+
+      $scope.addMeal = function() {
+        $scope.meals.unshift({
+          name: $scope.name,
+          calories: $scope.calories,
+          eatenAt: $scope.eatenAt
+        });
+
+        $scope.name = '';
+        $scope.calories = '';
+        $scope.eatenAt = '';
+      };
     }
   ]);
