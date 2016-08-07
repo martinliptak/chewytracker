d476bedcbda75b85854ccea5e809c6ed226bd130
Martin Lipták <mliptak@gmail.com>
2016-08-07 18:20:14 +0200

    Add service

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -1,16 +1,23 @@
 angular
   .module('chewyTracker', [])
-  .controller('DashboardCtrl', [
-    '$scope',
-    function($scope) {
-      $scope.meals = [
+  .factory('meals', [function() {
+    return {
+      meals: [
         {name: 'Dinner on Saturday', calories: 250, eatenAt: '06 Aug 18:00'},
         {name: 'Lunch on Saturday', calories: 250, eatenAt: '06 Aug 12:00'},
         {name: 'Breakfast on Saturday', calories: 250, eatenAt: '06 Aug 09:00'},
         {name: 'Dinner on Friday', calories: 250, eatenAt: '06 Aug 18:00'},
         {name: 'Lunch on Friday', calories: 250, eatenAt: '06 Aug 12:00'},
         {name: 'Breakfast on Friday', calories: 250, eatenAt: '06 Aug 09:00'}
-      ];
+      ]
+    };
+  }])
+  .controller('DashboardCtrl', [
+    '$scope',
+    'meals',
+    function($scope, meals) {
+      $scope.meals = meals.meals;
+      console.log($scope.meals);
 
       $scope.addMeal = function() {
         $scope.meals.unshift({
